8a35420807737781d283f5fa1be2af7b33d423dd
sliesarchukDiana <slesarcukdiana8@gmail.com>
2025-09-22 16:29:12 +0300

    Add new solution to task2 (Python)

---
+++ labs/lab-1/task1.2_Python/main.py
@@ -0,0 +1,37 @@
+import random
+
+def check_point(x, y):
+    parabola = -(x * x) + 2
+    diagonal= x
+
+    if x >= 0: #right side
+        inside = parabola > y > 0 and y < diagonal
+        on_border = y == diagonal or y == parabola
+    else: #left side (x<= 0)
+        inside = diagonal < y < parabola and y <= 0
+        on_border = y == diagonal or y == parabola
+
+    if inside:
+        return "потрапив в мішень"
+    elif on_border:
+        return "на межі мішені"
+    else:
+        return "мішень не ушкоджена"
+
+def generate_shots(n):
+    shots = []
+    for _ in range(n):
+        x = round(random.uniform(-2, 2), 2)   # координати від -5 до 5
+        y = round(random.uniform(-2, 2), 2)
+        shots.append((x, y))
+    return shots
+
+def main():
+    shots = generate_shots(10)
+    print(f"{'№ пострілу':<10} {'Координати':<20} {'Результат'}")
+    for i, (x, y) in enumerate(shots, start=1):
+        result = check_point(x, y)
+        print(f"{i:<10} ({x:>5}, {y:>5}){'':<5} {result}")
+
+if __name__ == "__main__":
+    main()
