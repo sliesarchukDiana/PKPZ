59e6ea6b957a1a66604c78a32069e62c773a6716
sliesarchukDiana <slesarcukdiana8@gmail.com>
2025-09-29 17:36:19 +0300

    Add solution to task1.2 lab-2 C#

---
+++ labs/lab-2/task1.2_1.4_C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿class ConsoleApp1
+{
+    static void Main()
+    {
+        int n = 6;
+        int[,] A = new int[n, n];
+        int[] X = new int[n];
+
+        Console.Write("Enter value of x: ");
+        int x = int.Parse(Console.ReadLine());
+        FillMatrix(A, -50, 49);
+
+        Console.WriteLine("Matrix A");
+        PrintMatrix(A);
+        
+        BuildVector(A, X, x);
+
+        Console.WriteLine("Logical vector X:");
+        PrintVector(X);
+    }
+    
+    static void FillMatrix(int[,] matrix, int min, int max)
+    {
+        Random rnd = new Random();
+        int n = matrix.GetLength(0);
+
+        for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            matrix[i, j] = rnd.Next(min, max + 1);
+    }
+    
+    static void PrintMatrix(int[,] matrix)
+    {
+        int n = matrix.GetLength(0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+                Console.Write($"{matrix[i, j],4}");
+            Console.WriteLine();
+        }
+    }
+    
+    static void BuildVector(int[,] matrix, int[] vector, int x)
+    {
+        int n = matrix.GetLength(0);
+
+        for (int i = 0; i < n; i++)
+        {
+            int maxInRow = int.MinValue;
+            for (int j = 0; j < n; j++)
+                if (matrix[i, j] > maxInRow)
+                    maxInRow = matrix[i, j];
+
+            vector[i] = (maxInRow <= x) ? 1 : 0;
+        }
+    }
+    
+    static void PrintVector(int[] vector)
+    {
+        for (int i = 0; i < vector.Length; i++)
+            Console.Write(vector[i] + " ");
+        Console.WriteLine();
+    }
+}
--- labs/lab-2/task1.2_1.4_C#/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ labs/lab-2/task1.2_1.4_C#/ConsoleApp1/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca94554d2d459e6ffa4232569597d82da9d997c")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
